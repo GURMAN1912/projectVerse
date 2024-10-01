31260743955225ebfcb7dd518ce583d3b19ae956
GURMAN1912 <manansinghsambhi@gmail.com>
2024-10-02 02:18:47 +0530

    final

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist', // this is default for Vite
+    rollupOptions: {
+      // any custom build options
+    }
+  },
   server:{
     proxy:{
       '/api':{
